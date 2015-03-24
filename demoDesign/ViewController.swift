79dfa82f450173ba1e5bc1e9ee0e16756250fa9a
sanbei120 <jonlinpei@gmail.com>
2015-03-24 10:53:08 -0700

    Initial Commit

---
+++ demoDesign/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  demoDesign
+//
+//  Created by Jonlin Pei on 3/9/15.
+//  Copyright (c) 2015 Jonlin Pei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var buttons: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
